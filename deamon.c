c4258ad899e8842dd127e56b3326fbedf9c3369b
Craig Van Tassle <craig@codestorm.org>
2009-09-30 04:20:02 +0000

    Setup serial to be 9600 Baud 8N1

--- deamon.c
+++ deamon.c
@@ -10,46 +10,88 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/signal.h>
-#include <sys/type.h>
+#include <sys/types.h>
 #include <termios.h>
+#include <errno.h>
+#include <stdlib.h>
 
+/* At some point I need to set this to be set via CLI or VIA a config file */
+#define SERIALDEV "/dev/ttyS0"
+void signal_handler_IO (int status);
 
-#DEFINE BAUD 9600
-#DEFINE SERIALDEV "/dev/ttyUSB0"
+int read_status = 0;
 
 int main(int argc, char *argv[])
 {
    int fd = 0;
    int res = 0;
+   int CMD = 0;
+   int pid;	
    struct termios ctl_port;
+   struct sigaction saio;
    char from_tv[20];
    char to_tv[20];
 
    /* Deamonize the Application */
    if ((pid= fork()) < 0 )
-   {  perror(Form);
+   {  perror("Fork");
       exit(errno);
    }
    else if ( pid != 0)
+   {
+      fputs("Exiting Parent\n", stderr);
       exit(0);
+   }
+   fputs("In Child/Deamon\n",stderr);
    
-   if ( (fd=open(SERIALDEV, O_RDWR | O_NOCTTY )) < 0)
+   /* install the signal handler before making the device asynchronous */
+   saio.sa_handler = signal_handler_IO;
+   /*saio.sa_mask = 0; Comile Error? */
+   saio.sa_flags = 0;
+   saio.sa_restorer = NULL;
+   sigaction(SIGIO,&saio,NULL);
+   /* allow the process to receive SIGIO */
+   fcntl(fd, F_SETOWN, getpid());
+   /* Make the file descriptor asynchronous (the manual page says only
+      O_APPEND and O_NONBLOCK, will work with F_SETFL...) */
+   fcntl(fd, F_SETFL, FASYNC);
+
+
+
+   if ( (fd=open(SERIALDEV, O_RDWR | O_NOCTTY | O_NONBLOCK )) < 0)
    {
       perror(SERIALDEV);
       exit -1;
    }
+   /* Set the Serial port Connection Paramaters  */
 
-   bzero(&ctl_port, sizeof(ctl_port));
-   ctl_port.c_flag = BAUD | CRTSCTS | CS8 | CLOCAL | CREAD;
-   ctl_port.c_iflag = IGNPAR;
-   ctl_port.c_oflag = 0;
-   ctl_port.c_lflag = 0;
-   ctl_port.c_cc[VTIME] = 0;
-   ctl_port.c_cc[VMIN] = 5;
-   tcflush(fd, TCIFLUH);
+   tcgetattr(fd, &ctl_port);
+   cfsetispeed(&ctl_port, B9600);                 /* Set the baud rates to 9600 */
+   cfsetospeed(&ctl_port, B9600);
+   ctl_port.c_cflag |= (CLOCAL | CREAD);
+   ctl_port.c_cflag &= ~PARENB; /* Mask the character size to 8 bits, no parity */
+   ctl_port.c_cflag &= ~CSTOPB;
+   ctl_port.c_cflag &= ~CSIZE;
+   ctl_port.c_cflag |=  CS8;                              /* Select 8 data bits */
+   ctl_port.c_cflag &= ~CRTSCTS;         
+   tcflush(fd, TCIFLUSH);
    tcsetattr(fd, TCSANOW,&ctl_port);
+   
+ 
 
+   while(1)
+   {
+      /* Read input from the outside and sent the proper command to the TV and verify that the tv gets the command properly */
+   }
+
+   /* We should never Reach this point */
+   close(fd);
+   exit(0);
 }
    
 
+void signal_handler_IO (int status)
+{
+
+}
 
